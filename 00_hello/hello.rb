982ee7a8448289e316e52dc613abdee93587d80b
Lauren Sheridan <lsheridan@elon.edu>
2018-10-02 23:18:57 +0000

    hello world complete

--- 00_hello/hello.rb
+++ 00_hello/hello.rb
@@ -1,1 +1,15 @@
-#write your code here
+#write your code here: program that says "hello, <name>" OR "Hello world" if not given a name
+
+
+def hello
+    name = gets.chomp
+    if name.length>0
+        puts "Hello, #{name}"
+    else
+        puts "Hello, World!"
+    end
+end
+
+
+puts "What's your name? "
+hello 
