0d4ec0d426e1cfa853b5e3948476cdea0dd3ea4d
Lauren Sheridan <lsheridan@elon.edu>
2018-10-03 01:19:24 +0000

    recommit

--- 00_hello/hello.rb
+++ 00_hello/hello.rb
@@ -1,15 +1,13 @@
 #write your code here: program that says "hello, <name>" OR "Hello world" if not given a name
 
-
 def hello
-    name = gets.chomp
-    if name.length>0
-        puts "Hello, #{name}"
-    else
-        puts "Hello, World!"
-    end
+    "Hello!"
 end
 
-
-puts "What's your name? "
 hello 
+
+def greet(name)
+    "Hello, #{name}!"
+end
+
+greet("Alice")
--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,30 @@
-#write your code here
+#write your code here: Write a program to convert fahrenheit to celsius. Test for boiling, freezing, body temp, and any other temp.
+#T(°C) = (T(°F) - 32) × 5/9
+#T(°F) = T(°C) × 9/5 + 32
+def ftoc(ftemp)
+    ftemp = ftemp.to_f
+    ctemp = (ftemp - 32) * (5.0/9.0)
+    print ctemp
+end
+
+def ctof(ctemp)
+   ctemp = ctemp.to_f
+   ftemp = (ctemp) * (9.0/5.0) + 32
+   print ftemp
+end
+
+ftoc(32)
+print " "
+ftoc(212)
+print " "
+ftoc(98.6)
+print " "
+ftoc(68)
+print " "
+ctof(0)
+print " "
+ctof(100)
+print " "
+ctof(20)
+print " "
+ctof(37)
